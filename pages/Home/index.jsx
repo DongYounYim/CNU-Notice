d89c0eef4707d5ba553902586ad09e4123dff859
bae-sh <hyeon1293@gmail.com>
2023-05-21 19:18:28 +0900

    chore : 메인 이미지 생성

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,14 +1,15 @@
-import * as React from "react";
-import { Text, View, StatusBar, Button } from "react-native";
+import * as React from 'react';
+import { Text, View, StatusBar, Button, StyleSheet } from 'react-native';
 
 export default function Home({ navigation }) {
   const handlePagination = () => {
-    navigation.navigate("Chat");
+    navigation.navigate('Chat');
   };
   return (
-    <View>
-      <Text>Home page</Text>
-      <Button title="Go to Chat Page" onPress={handlePagination} />
+    <View styles={styles.view}>
+      <Text styles={styles}>Home page</Text>
+      <Image source={require('../../assets/main.jpeg')} />
+      <Button title="Go to Chat Page" onPress={handlePagination} styles={styles.navigatorButton} />
       <StatusBar style="auto" />
     </View>
   );
