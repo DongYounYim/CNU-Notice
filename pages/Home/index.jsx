1a0ef6a455f778e328bd1f0fd21c697703e605b1
bae-sh <hyeon1293@gmail.com>
2023-05-21 19:50:29 +0900

    chore : 메인 이미지 뷰 적용

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,16 +1,38 @@
 import * as React from 'react';
-import { Text, View, StatusBar, Button, StyleSheet } from 'react-native';
+import { Text, View, StatusBar, Button, StyleSheet, Image, Dimensions } from 'react-native';
 
 export default function Home({ navigation }) {
   const handlePagination = () => {
     navigation.navigate('Chat');
   };
+  const { height, width } = Dimensions.get('window');
+
   return (
-    <View styles={styles.view}>
-      <Text styles={styles}>Home page</Text>
-      <Image source={require('../../assets/main.jpeg')} />
-      <Button title="Go to Chat Page" onPress={handlePagination} styles={styles.navigatorButton} />
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View style={styles.imageView}>
+        <Image style={styles.image} source={require('../../assets/main.jpeg')} />
+      </View>
+
+      {/* <Button title="Go to Chat Page" onPress={handlePagination} /> */}
+      {/* <StatusBar /> */}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'stretch',
+  },
+  imageView: {
+    width: '100%',
+    height: '90vh',
+  },
+});
