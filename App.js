c114de1dc66e769b44d083886db09b8237344ad7
DongyounYim <dyyim4725@gmail.com>
2023-05-21 10:49:15 +0900

    [feat] 공지방 헤더 스타일 완성

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
 import Follow from "./pages/Follow";
+import { TouchableOpacity } from "react-native-web";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +15,35 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+          options={({ navigation }) => ({
+            headerTitle: ({ children }) => (
+              <View>
+                <Text style={{ fontSize: 18, fontWeight: "bold" }}>공지방</Text>
+              </View>
+            ),
+            headerRight: (props) => (
+              <TouchableOpacity>
+                <Icon name="add-circle-outline" size={30} />
+              </TouchableOpacity>
+            ),
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.pop()}>
+                <Icon name="arrow-back" size={30} />
+              </TouchableOpacity>
+            ),
+            headerStyle: {
+              margin: "8px 8px",
+            },
+          })}
+        />
         <Stack.Screen name="Follow" component={Follow} />
       </Stack.Navigator>
     </NavigationContainer>
--- pages/Chat/index.jsx
+++ pages/Chat/index.jsx
@@ -4,8 +4,7 @@ import { Text, View, StatusBar } from "react-native";
 export default function Chat() {
   return (
     <View>
-      <Text>Chat page</Text>
-      <StatusBar style="auto" />
+      <Text>공지방</Text>
     </View>
   );
 }
