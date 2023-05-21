544dd1d1d53488d68cf1aba9ba5112fe2454a374
bae-sh <hyeon1293@gmail.com>
2023-05-21 22:00:53 +0900

    feat : 수평선 컴포넌트 생성

---
+++ components/HorizonLine/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HorizonLine = ({ text }) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        textAlign: 'center',
+        borderBottom: '1px solid lightgray',
+        lineHeight: '0.1em',
+        margin: '10px 0 20px',
+      }}
+    ></div>
+  );
+};
+
+export default HorizonLine;
--- pages/Follow/index.jsx
+++ pages/Follow/index.jsx
@@ -1,11 +1,16 @@
-import * as React from "react";
-import { Text, View, StatusBar } from "react-native";
+import * as React from 'react';
+import { Text, View, StatusBar } from 'react-native';
+import { HorizonLine } from '../../components';
 
 export default function Follow() {
+  const [myNotice, setMyNotice] = React.useState([]);
   return (
     <View>
-      <Text>Follow page</Text>
-      <StatusBar style="auto" />
+      <Text style={{ paddingLeft: '10px' }}>
+        <h3>나의 공지사항</h3>
+      </Text>
+
+      <HorizonLine />
     </View>
   );
 }
