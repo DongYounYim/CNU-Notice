ffdc7a76946fa1ffb6b9238618c84119cf2f7fc9
bae-sh <hyeon1293@gmail.com>
2023-05-21 21:48:52 +0900

    feat : 네비게이션 바 생성

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -1,19 +1,101 @@
 import * as React from 'react';
-import { Text, View, StatusBar, Button, StyleSheet, Image, Dimensions } from 'react-native';
+import {
+  Text,
+  View,
+  StatusBar,
+  Button,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import Chat from '../Chat';
 export default function Home({ navigation }) {
   const handlePagination = () => {
     navigation.navigate('Chat');
   };
-  const { height, width } = Dimensions.get('window');
+  const Tab = createBottomTabNavigator();
 
   return (
     <View style={styles.container}>
       <View style={styles.imageView}>
         <Image style={styles.image} source={require('../../assets/main.jpeg')} />
       </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          height: '10vh',
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: '20%',
+            height: '10vh',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+          }}
+        >
+          <MaterialCommunityIcons name="home" size={30} />
+          <Text>메인</Text>
+        </TouchableOpacity>
 
-      {/* <Button title="Go to Chat Page" onPress={handlePagination} /> */}
+        <TouchableOpacity
+          style={{
+            width: '20%',
+            height: '10vh',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+          }}
+        >
+          <MaterialCommunityIcons name="message-outline" size={30} />
+          <Text>메신저</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{
+            width: '20%',
+            height: '10vh',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+          }}
+          onPress={handlePagination}
+        >
+          <MaterialCommunityIcons name="bullhorn-variant-outline" size={30} />
+          <Text>공지</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: '20%',
+            height: '10vh',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+          }}
+        >
+          <MaterialCommunityIcons name="town-hall" size={30} />
+          <Text>백마인턴십</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            width: '20%',
+            height: '10vh',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+          }}
+        >
+          <MaterialCommunityIcons name="book-open-page-variant-outline" size={30} />
+          <Text>도서관</Text>
+        </TouchableOpacity>
+      </View>
+
       {/* <StatusBar /> */}
     </View>
   );
@@ -26,6 +108,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+
   image: {
     width: '100%',
     height: '100%',
@@ -34,5 +117,8 @@ const styles = StyleSheet.create({
   imageView: {
     width: '100%',
     height: '90vh',
+  },
+  navigationTab: {
+    backgroundColor: 'white',
   },
 });
