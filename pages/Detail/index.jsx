c513c0ad8950e7ddbf0905ca06280e3808e41d97
DongyounYim <dyyim4725@gmail.com>
2023-06-08 00:01:38 +0900

    [Add] Detail 페이지 틀 추가

--- pages/Detail/index.jsx
+++ pages/Detail/index.jsx
@@ -1,13 +1,62 @@
 import React from "react";
-import { View, Text } from "react-native-web";
+import { ScrollView, View, Text, useWindowDimensions } from "react-native";
+import RenderHTML from "react-native-render-html";
 
 export default function Detail({ route }) {
+  const { width } = useWindowDimensions();
   const { title, content, writer } = route.params;
+  const source = {
+    html: content,
+  };
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{writer}</Text>
-      <View>{content}</View>
-    </View>
+    <ScrollView>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          border: "1px solid black",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: "1.5rem",
+            fontWeight: "600",
+            minWidth: "max-content",
+            padding: "8px",
+            borderRight: "1px solid black",
+          }}
+        >
+          제목
+        </Text>
+        <Text style={{ fontSize: "1.2rem", fontWeight: "600", padding: "4px" }}>
+          {title}
+        </Text>
+      </View>
+      <RenderHTML contentWidth={width} source={source} />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          border: "1px solid black",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: "1.5rem",
+            fontWeight: "600",
+            minWidth: "max-content",
+            padding: "8px",
+            borderRight: "1px solid black",
+          }}
+        >
+          작성자
+        </Text>
+        <Text style={{ fontSize: "1.2rem", fontWeight: "600", padding: "4px" }}>
+          {writer}
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
