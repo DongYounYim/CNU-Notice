1158f145265e59ac35a6fb9218aac098feb015de
bae-sh <hyeon1293@gmail.com>
2023-06-12 15:14:05 +0900

    chore : 홈 화면 라우팅

--- App.js
+++ App.js
@@ -1,50 +1,43 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import Chat from "./pages/Chat";
-import Home from "./pages/Home";
-import Follow from "./pages/Follow";
-import ChatRoom from "./pages/ChatRoom";
-import { TouchableOpacity, Pressable } from "react-native";
-import { saveMyNotices } from "./api";
-import Detail from "./pages/Detail";
+import Chat from './pages/Chat';
+import Home from './pages/Home';
+import Follow from './pages/Follow';
+import ChatRoom from './pages/ChatRoom';
+import { TouchableOpacity, Pressable } from 'react-native';
+import { saveMyNotices } from './api';
+import Detail from './pages/Detail';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Chat">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen
           name="Chat"
           component={Chat}
           options={({ navigation }) => ({
             headerTitle: () => (
               <View>
-                <Text style={{ fontSize: 18, fontWeight: "bold" }}>공지방</Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>공지방</Text>
               </View>
             ),
             headerRight: () => (
               <TouchableOpacity
-                style={{ paddingRight: "8px" }}
-                onPress={() => navigation.navigate("Follow")}
+                style={{ paddingRight: '8px' }}
+                onPress={() => navigation.navigate('Follow')}
               >
                 <Icon name="add-circle-outline" size={30} />
               </TouchableOpacity>
             ),
             headerLeft: () => (
-              <TouchableOpacity
-                style={{ paddingLeft: "8px" }}
-                onPress={() => navigation.pop()}
-              >
+              <TouchableOpacity style={{ paddingLeft: '8px' }} onPress={() => navigation.pop()}>
                 <Icon name="arrow-back" size={30} />
               </TouchableOpacity>
             ),
@@ -56,17 +49,15 @@ export default function App() {
           options={({ navigation, route: { params } }) => ({
             headerTitle: () => (
               <View>
-                <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                  충남대학교 공지사항
-                </Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>충남대학교 공지사항</Text>
               </View>
             ),
             headerRight: () => (
               <Pressable
                 style={{
-                  border: "1px solid black",
-                  padding: "8px",
-                  marginRight: "8px",
+                  border: '1px solid black',
+                  padding: '8px',
+                  marginRight: '8px',
                 }}
                 onPress={async () => {
                   await saveMyNotices(Object.values(params.myNotice));
@@ -77,10 +68,7 @@ export default function App() {
               </Pressable>
             ),
             headerLeft: () => (
-              <TouchableOpacity
-                style={{ paddingLeft: "8px" }}
-                onPress={() => navigation.pop()}
-              >
+              <TouchableOpacity style={{ paddingLeft: '8px' }} onPress={() => navigation.pop()}>
                 <Icon name="arrow-back" size={30} />
               </TouchableOpacity>
             ),
@@ -95,10 +83,10 @@ export default function App() {
                 <Text
                   style={{
                     fontSize: 18,
-                    fontWeight: "bold",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    whiteSpace: "nowrap",
+                    fontWeight: 'bold',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
                   }}
                 >
                   {route.params.title}
@@ -106,10 +94,7 @@ export default function App() {
               </View>
             ),
             headerLeft: () => (
-              <TouchableOpacity
-                style={{ paddingLeft: "8px" }}
-                onPress={() => navigation.pop()}
-              >
+              <TouchableOpacity style={{ paddingLeft: '8px' }} onPress={() => navigation.pop()}>
                 <Icon name="arrow-back" size={30} />
               </TouchableOpacity>
             ),
@@ -124,10 +109,10 @@ export default function App() {
                 <Text
                   style={{
                     fontSize: 18,
-                    fontWeight: "bold",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    whiteSpace: "nowrap",
+                    fontWeight: 'bold',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    whiteSpace: 'nowrap',
                   }}
                 >
                   {route.params.title}
@@ -135,10 +120,7 @@ export default function App() {
               </View>
             ),
             headerLeft: () => (
-              <TouchableOpacity
-                style={{ paddingLeft: "8px" }}
-                onPress={() => navigation.pop()}
-              >
+              <TouchableOpacity style={{ paddingLeft: '8px' }} onPress={() => navigation.pop()}>
                 <Icon name="arrow-back" size={30} />
               </TouchableOpacity>
             ),
